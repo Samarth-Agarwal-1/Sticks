8861d734b5d4c5212d0b0d3e69ec16716fea1c8b
Samarth-Agarwal-1 <40373256+Samarth-Agarwal-1@users.noreply.github.com>
2020-08-20 12:29:07 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+		Scanner reader = new Scanner(System.in);
+		int sticks = (int)(Math.random()*10) + 21;
+		System.out.println("There are " + sticks + " sticks.");
+		System.out.println("Enter 1 to go first and 2 to go second.");
+		boolean turn; //turn true is player turn, false is computer turn
+		if(reader.nextInt() == 1)
+		    turn = true;
+		else
+		    turn = false;
+		    
+		//game stops when sticks = 0
+		//game must keep running as long as sticks are greater than 10
+		
+		while (sticks > 0){
+		    if(turn)
+		        sticks = playerMove(sticks, reader);
+		    else
+		        sticks = computerMove(sticks);
+		    System.out.println("There are " + sticks + " left");
+		    if(sticks == 0 && turn)
+		        System.out.println("Player wins!");
+		    if(sticks == 0 && !turn)
+		        System.out.println("Computer wins!");
+		    turn = !turn;
+		}
+	}
+	
+	public static int playerMove(int sticks, Scanner reader){
+	    System.out.println("Select how many sticks to take");
+	    int take = reader.nextInt();
+	    if(take < 1)
+	        take = 1;
+	    if(take > 3)
+	        take = 3;
+	    else if(take > sticks)
+	        take = sticks;
+	    System.out.println("You take " + take + " sticks.");
+	    return sticks - take;
+	}
+	
+	public static int computerMove(int sticks){
+	    int take;
+	    if(sticks % 4 != 0)
+	        take = sticks % 4;
+	    else
+	        take = (int)(Math.random()*3) + 1;
+	    System.out.println("The computer took " + take + " sticks.");
+	    return sticks - take;
+	}
+}
